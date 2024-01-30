c1178cb36e655c967b56b9514247457c1abf650f
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2024-01-31 00:11:39 +0100

    add fzf wrapper

--- setup.ps1
+++ setup.ps1
@@ -7,3 +7,4 @@ New-Item -ItemType Junction -Path "$env:localappdata\nvim" -Target "$dotfiles\nv
 New-Item -ItemType SymbolicLink -Path "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\remap.ahk" -Target "$dotfiles\remap.ahk"
 New-Item -ItemType SymbolicLink -Path "$env:userprofile\.gitconfig" -Target "$dotfiles\.gitconfig"
 New-Item -ItemType SymbolicLink -Path "$env:userprofile\.wezterm.lua" -Target "$dotfiles\.wezterm.lua"
+New-Item -ItemType SymbolicLink -Path "$env:userprofile\bin\fs.ps1" -Target "$dotfiles\fs.ps1"
