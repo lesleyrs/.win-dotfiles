94187997fff966fde88d4629d86b768fcd7f1b5b
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2024-04-13 15:48:52 +0200

    rm eza

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,12 @@
 Set-Alias nv nvim -Force
 Set-Alias gu gitui -Force
 Set-Alias lg lazygit
-Set-Alias ll eza
+Set-Alias l ls
 
 Set-Alias gc GitClone -Force
 Set-Alias c CDDoubleDot
 Set-Alias e ExplorerDot
 Set-Alias u WingetUpdate
-Set-Alias l EzaALH
 
 function CDDoubleDot {
 	cd..
@@ -23,8 +22,4 @@ function WingetUpdate {
 
 function GitClone {
 	git clone $args
-}
-
-function EzaALH {
-	eza -alh $args
 }
