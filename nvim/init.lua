37f78379c59e1dd18149151783c386c982222116
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2024-10-08 15:15:13 +0200

    fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,11 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'c',
